27da303004017d36febb071333d34bbaf4561d0c
lauuu-pt <103839621+lauuu-pt@users.noreply.github.com>
2022-11-17 15:59:13 +0000

    Add files via upload

---
+++ assets/js/src/main.js
@@ -0,0 +1,178 @@
+
+//   return (shop.innerHTML = shopItemsData
+//     .map((x) => {
+//       let { id, name, price, desc, img } = x;
+//       let search = basket.find((x) => x.id === id) || [];
+//       return `
+//           <div class="col-lg-4 col-sm-6 dish-box-wp ${categoria} data-cat="">
+//             <div id=product-id-${id} class="item">
+//               <div class="dish-box text-center">
+//                   <div class="dist-img">
+//                       <img src=${img} alt="">
+//                   </div>
+//                   <div class="dish-title">
+//                       <h3 class="h3-title">${name}</h3>
+                  
+//                   </div>
+//                   <div class="dish-info">
+//                       <ul>
+//                           <li>
+//                               <p>Quantidade</p>
+                          
+//                           </li>
+//                           <li>
+//                               <b>${quantidade}</b>
+
+//                           </li>
+//                       </ul>
+//                   </div>
+//                   <div class="dist-bottom-row">
+//                       <ul>
+//                           <li>
+//                               <button class="dish-add-btn">
+//                                   <i onclick="increment(${id}) class="uil uil-plus"></i>
+//                                   <div id=${id} class="quantity">
+//                                   ${search.item === undefined ? 0 : search.item}
+//                                   </div>
+//                               </button>
+//                           </li>
+//                           <li>
+//                               <b> ${price} €</b>
+//                           </li>
+                          
+//                           <li>
+                              
+//                           <a href="#" class="btn-menu">More info</a>
+//                           </li>
+               
+//                       </ul>
+//                   </div>
+//               </div>
+//             </div>
+//           </div>
+
+//     `;
+//     })
+//     .join(""));
+// };
+
+
+
+let shop = document.getElementById("shop");
+
+let basket = JSON.parse(localStorage.getItem("data")) || [];
+
+let generateShop = () => {
+  return (shop.innerHTML = shopItemsData
+    .map((x) => {
+      let { id, name, price, categoria, img } = x;
+      let search = basket.find((x) => x.id === id) || [];
+      return `
+
+     
+    <div id=product-id-${id} class="item"> 
+    <div class="dish-box text-center">
+          <div class="dist-img">
+              <img src=${img} alt="">
+          </div>
+          <div class="dish-title">
+              <h3 class="h3-title">${name}</h3>
+          
+          </div>
+          
+          <div class="dish-info">
+              <ul>
+              <li>
+              <p>Preço</p>
+
+              </li>
+                  <li>
+                      <b>${price} €</b>
+
+                  </li>
+              </ul>
+          </div>
+            <div class="dist-bottom-row">
+            <ul>
+            <li>
+              <i onclick="decrement(${id})" class="uil uil-minus"></i>
+              <div id=${id} class="quantity">
+              ${search.item === undefined ? 0 : search.item}
+              </div>
+              <i onclick="increment(${id})" class="uil uil-plus"></i>
+            </li>
+
+            <li>
+              <a href="#" class="btn-menu">+ info</a>
+            </li>
+            </ul>
+
+            </div>
+          
+            </div>
+      </div>
+    `;
+    })
+    .join(""));
+};
+
+generateShop();
+
+let increment = (id) => {
+  let selectedItem = id;
+  let search = basket.find((x) => x.id === selectedItem.id);
+
+  if (search === undefined) {
+    basket.push({
+      id: selectedItem.id,
+      item: 1,
+    });
+  } else {
+    search.item += 1;
+  }
+
+  // console.log(basket);
+  update(selectedItem.id);
+  localStorage.setItem("data", JSON.stringify(basket));
+};
+let decrement = (id) => {
+  let selectedItem = id;
+  let search = basket.find((x) => x.id === selectedItem.id);
+
+  if (search === undefined) return;
+  else if (search.item === 0) return;
+  else {
+    search.item -= 1;
+  }
+  update(selectedItem.id);
+  basket = basket.filter((x) => x.item !== 0);
+  // console.log(basket);
+  localStorage.setItem("data", JSON.stringify(basket));
+};
+let update = (id) => {
+  let search = basket.find((x) => x.id === id);
+  // console.log(search.item);
+  document.getElementById(id).innerHTML = search.item;
+  calculation();
+};
+
+let calculation = () => {
+  let cartIcon = document.getElementById("cartAmount");
+  cartIcon.innerHTML = basket.map((x) => x.item).reduce((x, y) => x + y, 0);
+};
+
+calculation();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
