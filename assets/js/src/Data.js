5c281396faa467157d4c5ac64aa5e50a30d09e0b
lauuu-pt <103839621+lauuu-pt@users.noreply.github.com>
2022-11-21 09:41:43 +0000

    Add files via upload

---
+++ assets/js/src/Data.js
@@ -0,0 +1,83 @@
+let shopItemsData = [
+  //entradas
+  {
+    id: "jfh",
+    name: "Missoshiro",
+    price: 2.5,
+    categoria:"entradas",
+    img: "assets/images/dish/4.png",
+  },
+  {
+    id: "ioy",
+    name: "Tempuras de Camarão",
+    price: 6.4,
+    categoria: "entradas",
+    img: "assets/images/dish/2.png",
+  },
+  {
+    id: "wue",
+    name: "Guiosas de Frango",
+    price: 6.2,
+    categoria:"entradas",
+    img: "assets/images/dish/3.png",
+  },
+  {
+    id: "thy",
+    name: "Guiosas Vegetarianas",
+    price: 6.0,
+    categoria: "entradas",
+    img: "assets/images/dish/3.png",
+  },
+
+  //sushi
+
+  //combinados
+  {
+    id: "sss",
+    name: "Combinado I",
+    price: 24.4,
+    categoria: "combinados",
+    img: "assets/images/dish/7.png",
+  },
+  {
+    id: "sdsr",
+    name: "Combinado II",
+    price: 26.2,
+    categoria: "combinados",
+    img: "assets/images/dish/8.png",
+  },
+  {
+    id: "sdef",
+    name: "Combinado de Gunkans",
+    price: 32.0,
+    categoria: "combinados",
+    img: "assets/images/dish/9.png",
+  },
+
+  
+
+  //bebidas
+  {
+    id: "dhb",
+    name: "Sumol de Laranja do Algarve",
+    price: 2.0,
+    categoria: "bebidas",
+    img: "assets/images/dish/1.png",
+  },
+  {
+    id: "sfe",
+    name: "Água Luso",
+    price: 1.6, 
+    categoria: "bebidas",
+    img: "assets/images/dish/11.png",
+  },
+  {
+    id: "add",
+    name: "Nestea de Pêssego",
+    price: 2.0,
+    categoria: "bebidas",
+    img: "assets/images/dish/12.png",
+  },
+
+  //sobremesas
+];
