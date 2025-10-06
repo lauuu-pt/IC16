26ad5aef072315b203608a3601eeff20a14847a3
Laura Luís Tomás <103839621+lauuu-pt@users.noreply.github.com>
2025-10-06 15:14:40 +0100

    Final updates

---
+++ README.txt
@@ -0,0 +1,16 @@
+# IC
+
+A sushi app that takes in consideration user interaction
+
+Use browser Brave ou Chrome ou Edge
+
+
+Start with inicio.html by clicking F5
+
+
+
+Made by:
+Bárbara 
+Laura 
+Mariana 
+Natacha 
---
+++ assets/js/cart.js
@@ -0,0 +1,163 @@
+let label = document.getElementById("label");
+let ShoppingCart = document.getElementById("shopping-cart");
+
+let basket = JSON.parse(localStorage.getItem("data")) || [];
+
+let calculation = () => {
+  let cartIcon = document.getElementById("cartAmount");
+  cartIcon.innerHTML = basket.map((x) => x.item).reduce((x, y) => x + y, 0);
+};
+
+calculation();
+
+let generateCartItems = () => {
+  if (basket.length !== 0) {
+    return (ShoppingCart.innerHTML = basket
+      .map((x) => {
+        let { id, item } = x;
+        let search = shopItemsData.find((y) => y.id === id) || [];
+        return `
+      <div class="cart-item">
+      <div class="cart-item-img">
+        <img src=${search.img} alt="">
+      </div>
+        <div class="details">
+
+          <div class="title-price-x">
+              <h4 class="title-price">
+                <p>${search.name}</p>
+                <p class="cart-item-price">${search.price} €</p>
+              </h4>
+              <i onclick="removeItem(${id})" class="uil uil-times"></i>
+          </div>
+
+          <div class="buttons">
+              <i onclick="decrement(${id})" class="uil uil-minus"></i>
+              <div id=${id} class="quantity">${item}</div>
+              <i onclick="increment(${id})" class="uil uil-plus"></i>
+          </div>
+
+         <!-- <h3> ${item * search.price} €</h3> -->
+        </div>
+      </div>
+      `;}).join(""));
+  } else {
+    ShoppingCart.innerHTML = ``;
+    label.innerHTML = `
+    <div class="empty">
+    <h2 class="h2-title">O cesto está vazio
+    <i class="uil uil-frown"></i></h2>
+    <a href="index.html">
+      <button class="HomeBtn">Home</button>
+    </a>
+    </div>
+    `;
+  }
+};
+
+generateCartItems();
+
+let increment = (id) => {
+  let selectedItem = id;
+  let search = basket.find((x) => x.id === selectedItem.id);
+
+  if (search === undefined) {
+    basket.push({
+      id: selectedItem.id,
+      item: 1,
+    });
+  } else {
+    search.item += 1;
+  }
+
+  generateCartItems();
+  update(selectedItem.id);
+  localStorage.setItem("data", JSON.stringify(basket));
+};
+let decrement = (id) => {
+  let selectedItem = id;
+  let search = basket.find((x) => x.id === selectedItem.id);
+
+  if (search === undefined) return;
+  else if (search.item === 0) return;
+  else {
+    search.item -= 1;
+  }
+  update(selectedItem.id);
+  basket = basket.filter((x) => x.item !== 0);
+  generateCartItems();
+  localStorage.setItem("data", JSON.stringify(basket));
+};
+
+let update = (id) => {
+  let search = basket.find((x) => x.id === id);
+  // console.log(search.item);
+  document.getElementById(id).innerHTML = search.item;
+  calculation();
+  TotalAmount();
+};
+
+let removeItem = (id) => {
+  let selectedItem = id;
+  // console.log(selectedItem.id);
+  basket = basket.filter((x) => x.id !== selectedItem.id);
+  generateCartItems();
+  TotalAmount();
+  localStorage.setItem("data", JSON.stringify(basket));
+};
+
+let clearCart = () => {
+  basket = [];
+  generateCartItems();
+  localStorage.setItem("data", JSON.stringify(basket));
+};
+function mostraModalDadosO() {
+  let modal = document.getElementById("modalDadosO");
+  let span = document.getElementsByClassName("fecharO")[0];
+  modal.style.display = "block";
+  span.addEventListener('click', function () {
+    modal.style.display = "none";
+  })}
+
+let TotalAmount = () => {
+  if (basket.length !== 0) {
+    let amount = basket
+      .map((x) => {
+        let { item, id } = x;
+        let search = shopItemsData.find((y) => y.id === id) || [];
+
+        return Math.round( item * search.price * 10 ) / 10;
+      })
+      .reduce((x, y) => x + y, 0);
+    // console.log(amount);
+    label.innerHTML = `
+    <div class="total">
+    <h2>Total: ${amount} €</h2>
+    <a class="removeAll" href="progresso.html"> Pedir</a>
+    
+    <a onclick="mostraModalDadosO()" class="removeAll">Oferecer</a>
+    
+    <div id="modalDadosO" class="modal1">
+    <!-- conteudo da caixa -->
+    <div class="modalConteudo1">
+      <span class="fecharO">&times;</span>
+        <p>Para que mesa deseja oferecer?</p>
+        <p>Total: ${amount} €<p>
+        <label for="txtNome"><strong>Número da mesa:</strong></label>
+        <input id="txtNome" type="int" name="nome" required maxlength="40" size="10">
+        <br><br>
+        <a class="removeAll" onclick="clearCart()" href="progresso.html"><strong>Confirmar</strong></a>
+        <a class="checkout"  href="cart.html"><strong>Cancelar</strong></a>
+    </div>
+    </div>
+    <button onclick="clearCart()" class="checkout">Apagar Carrinho</button>
+    </div>
+
+    `;
+  } else return;
+};
+
+
+
+
+TotalAmount();
